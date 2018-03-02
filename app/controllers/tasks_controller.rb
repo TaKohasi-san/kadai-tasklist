1a7641e83d9698685da6b3c145caeb7dfba1b464
EC2 Default User <ec2-user@ip-172-31-17-138.us-east-2.compute.internal>
2018-03-02 11:42:45 +0000

    bootstrap design

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = Task.all
+    @tasks = Task.order(created_at: :desc).page(params[:page]).per(3)
   end
   
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -26,7 +26,6 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
@@ -52,6 +51,10 @@ class TasksController < ApplicationController
   end
 
   private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
   
   # Strong Parameter
   def task_params
